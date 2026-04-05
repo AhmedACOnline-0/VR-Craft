559ca3da1828923248aafebecdfd54b00b8d1100
AhmedACOnline-0 <ahmedac6889@gmail.com>
2026-04-05 22:22:24 +0200

    G

---
+++ Project.js
@@ -0,0 +1,8 @@
+// project.
+const condition = true;
+if (condition == true) {
+  console.log("Condition/If is working.");
+}
+// for modrinth.
+const projectName = "VR Craft";
+const projectVersion = 1.
